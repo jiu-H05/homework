338f52dc36d415fd262059e6038f990f6f56f35f
lms <lms@local>
2026-09-21 21:44:04 +0800

    chore: 清理构建产物与临时测试文件

--- tools/dllexp/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul
-cd /d "C:\Users\Lenovo\Desktop\PJ\lms-modern\tools\dllexp"
-cl /nologo /LD mydll.c
-cl /nologo myexe.c /link mydll.lib
-echo BUILD_DONE
